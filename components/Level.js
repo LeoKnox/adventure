4a35f4df330935741b215b57b4af69dc4f4a7d2a
LeoKnox <LeoKnox@users.noreply.github.com>
2024-06-01 11:07:02 -0700

    Update Level.js

--- components/Level.js
+++ components/Level.js
@@ -1,9 +1,10 @@
-
 import { roomData } from "./roomData.js";
+import { Tile } from "./Tile.js";
 
 export default Level = () => {
   return <h3>level page</h3>;
   {
     roomData.name;
   }
+  <roomData.name />;
 };
