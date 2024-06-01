5c86d4510518cb8255eb7859c8dc646a7070e91e
LeoKnox <LeoKnox@users.noreply.github.com>
2024-06-01 10:45:51 -0700

    Create App.js

---
+++ App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import { level } from "./components/Level.js";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Adventure attempt again</h1>
+      <Level />
+    </div>
+  );
+}
