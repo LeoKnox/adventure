d43e7ce82ad956b64fccbdfe5f5cb46310a53538
LeoKnox <LeoKnox@users.noreply.github.com>
2024-06-02 17:23:58 -0700

    Update Level.js

--- components/Level.js
+++ components/Level.js
@@ -2,20 +2,17 @@ import { roomData } from "./roomData.js";
 import { tile } from "./Tile.js";
 
 export default Level = () => {
-  roomData.map((room) => {
-    console.log(`room ${room}`);
-  });
   return (
     <>
       <h3>level page</h3>
       <table>
-      {roomData.map((y, iy) => (
-      <tr>
-        {y.map((x, ix) => (
-          <td id={iy + "-" + ix}>{x.icon}</td>
+        {roomData.slice(1,roomData.length-1).map((y, iy) => (
+          <tr>
+            {y.slice(1, y.length-1).map((x, ix) => (
+              <td id={iy + "-" + ix}>{x.icon}</td>
+            ))}
+          </tr>
         ))}
-      </tr>
-    ))}
       </table>
     </>
   );
