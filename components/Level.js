44da3b341db4c60aa2b4bded68dfef56b4e5cd8a
LeoKnox <LeoKnox@users.noreply.github.com>
2024-06-02 17:25:59 -0700

    Update Level.js

--- components/Level.js
+++ components/Level.js
@@ -2,13 +2,15 @@ import { roomData } from "./roomData.js";
 import { tile } from "./Tile.js";
 
 export default Level = () => {
+  let maxWidth = roomData.length -1;
+  let maxLength = roomData[0].length-1;
   return (
     <>
       <h3>level page</h3>
       <table>
-        {roomData.slice(1,roomData.length-1).map((y, iy) => (
+        {roomData.slice(1, maxWidth).map((y, iy) => (
           <tr>
-            {y.slice(1, y.length-1).map((x, ix) => (
+            {y.slice(1, maxLength).map((x, ix) => (
               <td id={iy + "-" + ix}>{x.icon}</td>
             ))}
           </tr>
