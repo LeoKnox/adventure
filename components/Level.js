c304c935c9514a8f6a2f69f10d61da3eb411f5be
LeoKnox <LeoKnox@users.noreply.github.com>
2024-06-02 17:31:25 -0700

    Update Level.js

--- components/Level.js
+++ components/Level.js
@@ -1,16 +1,19 @@
+import {useState} from "react";
 import { roomData } from "./roomData.js";
 import { tile } from "./Tile.js";
 
 export default Level = () => {
-  let maxWidth = roomData.length -1;
-  let maxLength = roomData[0].length-1;
+  const [maxWidth, setMaxWidth] = useState(roomData.length - 1);
+  const [maxLength, setMaxLength] = useState(roomData[0].length - 1);
+  const [minWidth, setMinWidth] = useState(1);
+  const [minLength, setMinLength] = useState(1);
   return (
     <>
       <h3>level page</h3>
       <table>
-        {roomData.slice(1, maxWidth).map((y, iy) => (
+        {roomData.slice(minWidth, maxWidth).map((y, iy) => (
           <tr>
-            {y.slice(1, maxLength).map((x, ix) => (
+            {y.slice(minLength, maxLength).map((x, ix) => (
               <td id={iy + "-" + ix}>{x.icon}</td>
             ))}
           </tr>
