030d1836479dd75c7913cbe82cca24d201b285e1
LeoKnox <LeoKnox@users.noreply.github.com>
2024-06-03 11:55:30 -0700

    Update Tile.js

--- components/Tile.js
+++ components/Tile.js
@@ -1,7 +1,15 @@
 export default Tile = ({ tileData, position }) => {
-  return (
-    <td id={position}>
-      <div className="mapTile">{tileData.icon}</div>
-    </td>
-  );
+  if (position !== "1x1") {
+    return (
+      <td id={position}>
+        <div className="mapTile">{tileData.icon}</div>
+      </td>
+    );
+  } else {
+    return (
+      <td id={position}>
+        <div className="mapTile">人</div>
+      </td>
+    );
+  }
 };
