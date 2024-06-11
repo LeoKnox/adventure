d8a93e1f1265a7c21c369e14185859895a8dbcc9
LeoKnox <LeoKnox@users.noreply.github.com>
2024-06-11 13:53:36 -0700

    Update FighterSkills.js

--- components/FighterSkills.js
+++ components/FighterSkills.js
@@ -1,11 +1,14 @@
 import { fighter } from "./heroData.js";
 
 export default FighterSkills = () => {
+  const redTest = () => {
+    return "R" + "eD";
+  };
   return (
     <div>
       <button>Attack</button>
       <button>Defend</button>
-      {fighter({param:"RED"})}
+      {fighter({ param: redTest })}
     </div>
   );
 };
