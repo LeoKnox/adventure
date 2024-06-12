14a951a5fe4ba71916a43d5675f586ae039c92a4
LeoKnox <LeoKnox@users.noreply.github.com>
2024-06-12 11:25:33 -0700

    Update heroData.js

--- components/heroData.js
+++ components/heroData.js
@@ -6,9 +6,11 @@ export const heroData = {
 
 const redClick = ({ param }) => {
   heroData.hp = heroData.hp - 8;
+  alert(heroData.hp);
+  return heroData;
 };
 
 export const fighter = (param) => {
-  return <button onClick={() => redClick(param)}>Reverse</button>;
+  return <button onClick={() => redClick(param)}>Reverse:{heroData.hp}</button>;
   //return <button onClick={() => redTest()}>Red</button>;
 };
