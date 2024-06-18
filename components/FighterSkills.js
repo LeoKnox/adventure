70adcb81ed415170ac3f3fd7058294e88e78f9e9
LeoKnox <LeoKnox@users.noreply.github.com>
2024-06-18 15:37:14 -0700

    Update FighterSkills.js

--- components/FighterSkills.js
+++ components/FighterSkills.js
@@ -1,15 +1,21 @@
+import { useState } from "react";
 import { fighter, redClick } from "./heroData.js";
 
 export default FighterSkills = () => {
   const test = "fighter";
+  const [akaHero, setAka] = useState({ name: "red", hp: 55 });
   const redTest = () => {
-    return "R" + "eD";
+    setAka({ name: akaHero.name, hp: akaHero.hp - 1 });
   };
   return (
     <div>
+      <p>
+        ## {akaHero.name} ## {akaHero.hp}
+      </p>
       <button>Attack</button>
-      <button>Defend</button>
-      <button onClick={redClick}>hit</button>
+      <button>Defended</button>
+      <button onClick={() => redTest()}>hit</button>
+      {fighter(6)}
     </div>
   );
 };
